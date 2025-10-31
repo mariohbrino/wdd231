db971388b316dc9ddb01a308075921d7052e9da3
Mario Brino <mariobrino@gmail.com>
2025-10-31 16:29:12 -0600

    feat: added logo, menu behavior for mobile, and updates

--- scripts/courses.js
+++ scripts/courses.js
@@ -176,5 +176,32 @@ const filterCoursesListen = () => {
   setActiveButton(allElement);
 };
 
+const toggleIcon = () => {
+  const openIcon = document.querySelector("#openIcon");
+  const closeIcon = document.querySelector("#closeIcon");
+  const menu = document.querySelector("#toogleMenu");
+
+  menu.classList.toggle("open");
+
+  openIcon.classList.toggle("open-icon", !menu.classList.contains("open"));
+  openIcon.classList.toggle("close-icon", menu.classList.contains("open"));
+  closeIcon.classList.toggle("close-icon", !menu.classList.contains("open"));
+  closeIcon.classList.toggle("open-icon", menu.classList.contains("open"));
+};
+
+const setupMenuClickHandler = () => {
+  const toogleMenu = document.querySelector("#toogleMenu");
+  const navigation = document.querySelector("nav");
+  
+  toogleMenu.addEventListener("click", () => {
+    toggleIcon();
+
+    navigation.classList.toggle("open");
+
+    navigation.classList.toggle("nav-hidden", !navigation.classList.contains("open"));
+  });
+};
+
 populateAllCourses();
 filterCoursesListen();
+setupMenuClickHandler();
