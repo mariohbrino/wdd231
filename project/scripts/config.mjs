061fdc34c6c463ce73e0f26eb064e6a4bd707acd
Mario Brino <mariobrino@gmail.com>
2025-12-06 01:49:08 -0700

    fix: resolve issue with config.json creation and path

--- project/scripts/config.mjs
+++ project/scripts/config.mjs
@@ -2,7 +2,7 @@ const mapboxToken = async () => {
  let token = import.meta.env?.VITE_MAPBOX_TOKEN || null;
  
  try {
-   const response = await fetch('../config.json');
+   const response = await fetch('./config.json');
    if (response.ok) {
      const config = await response.json();
      if (config.mapbox_token && config.mapbox_token !== "YOUR_MAPBOX_TOKEN_HERE") {
